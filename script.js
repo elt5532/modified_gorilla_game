2474733bdbb5cc83a3740eea8105b399eead451e
elt5532 <elt5532@g.rit.edu>
2026-09-17 10:35:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -605,11 +605,11 @@ function drawGorillaThoughtBubbles(player) {
       ctx.save();
       ctx.scale(1, -1);
 
-      ctx.font = "20px sans-serif";
+      ctx.font = "50px sans-serif";
       ctx.textAlign = "center";
       ctx.fillText("?", 0, -90);
 
-      ctx.font = "10px sans-serif";
+      ctx.font = "30px sans-serif";
 
       ctx.rotate((5 / 180) * Math.PI);
       ctx.fillText("?", 0, -90);
