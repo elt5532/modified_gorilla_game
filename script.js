11726e2cee54b0a65c0bbbfd98b4225728093df7
elt5532 <elt5532@g.rit.edu>
2026-09-17 10:18:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -527,7 +527,7 @@ function drawGorillaLeftArm(player) {
 }
 
 function drawGorillaRightArm(player) {
-  ctx.strokeStyle = "black";
+  ctx.strokeStyle = "white";
   ctx.lineWidth = 18;
 
   ctx.beginPath();
